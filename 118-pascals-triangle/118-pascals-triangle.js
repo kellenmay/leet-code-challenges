a4e513591df22dad17381d67006726ca510f45bb
Kellen May <69639791+kellenmay@users.noreply.github.com>
2022-01-12 18:53:09 -0500

    Time: 156 ms (5.14%), Space: 38.3 MB (89.46%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+   let triangle = new Array(numRows)
+   
+   for( let i=0; i<numRows; i++){
+   let row = new Array(i+1)
+    row[0] = 1
+    row[row.length-1] = 1
+ 
+    for (let j=1; j<row.length-1; j++){
+        let prevRow = triangle[i-1]
+        row[j] = prevRow[j] + prevRow[j-1]
+    }
+    triangle[i] = row
+
+
+}
+    return triangle
+};
